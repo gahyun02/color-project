4f0d223850c6b68e0b5326b9a21ec872aa995154
이가현 <igh2468@likelion.org>
2023-10-19 18:30:15 +0900

    initial setting

---
+++ src/main/java/com/example/ColorProject/ColorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ColorProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ColorProject/ColorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ColorProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
